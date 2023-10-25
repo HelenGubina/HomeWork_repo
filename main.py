c24770f0a2ce102e3c4a6c55fa3a1d67b442efcf
Helen <gubinaelenaaleksandrovna@gmail.com>
2023-10-25 21:05:57 +0300

    Second task

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 
-word = "summer"
+# word = "summer"
+# try:
+#     print(word[3])
+#     print(word[6])
+# except IndexError:
+#     print("incorrect index")
+#
+# a = input()
 try:
-    print(word[3])
-    print(word[6])
-except IndexError:
-    print("incorrect index")
+    a = float(input("Input number 1: "))
+    b = float(input("Input number 2: "))
+except ValueError:
+    print("incorrect value")
+
