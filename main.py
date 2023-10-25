8f9ff6c2fb05eb162889d5756318868058e01c8f
Helen <gubinaelenaaleksandrovna@gmail.com>
2023-10-25 22:40:32 +0300

    Seventh task

--- main.py
+++ main.py
@@ -10,7 +10,9 @@
 import os
 
 fpath = os.path.join("e:", "text.txt")
-f = open(fpath, "w")
-mystr = str(input("input your string:"))
-f.write(mystr)
+f = open(fpath, "r")
+fpath1 = os.path.join("e:", "text1.txt")
+f1 = open(fpath1, "w")
+f1.write(f.read())
 f.close()
+f1.close()
