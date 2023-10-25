e57ee35e21707d6a356a679a6d78cbccc42e3018
Helen <gubinaelenaaleksandrovna@gmail.com>
2023-10-25 22:32:26 +0300

    Fifth task

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+#
+# a = float(input("input number 1:"))
+# b = float(input("input number 2:"))
+# try:
+#     c = a / b
+#     print(c)
+# except ZeroDivisionError:
+#     print("You can divide by zero")
 
-a = float(input("input number 1:"))
-b = float(input("input number 2:"))
-try:
-    c = a / b
-    print(c)
-except ZeroDivisionError:
-    print("You can divide by zero")
+import os
+
+fpath = os.path.join("e:", "text.txt")
+f = open(fpath, "r")
+mystr = f.read()
+mylist = mystr.split()
+print(len(mylist))
